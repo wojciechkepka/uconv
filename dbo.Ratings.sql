bae1808104281c5f5ee85d9a350ddc7f1e81123e
wojciechkepka <wojtek.kepka@protonmail.com>
2021-01-15 20:25:33 +0100

    Delete redundant files

--- dbo.Ratings.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Ratings] (
-    [Id]     INT           NOT NULL IDENTITY,
-    [date]   DATETIME      NOT NULL,
-    [name]   VARCHAR (250) NOT NULL,
-    [rating] INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
